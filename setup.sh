473505025f96b46a8a4aaeac3aae486ab4a11839
Jordan Bottoms <jbttoms@gmail.com>
2019-09-07 01:14:20 -0500

    fixed the setup.sh issues from downloads branch in the 101.4 branch
    Signed-off-by: Jordan Bottoms <jbttoms@gmail.com>

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ sudo mkdir /usr/share/clamav
 sudo groupadd clamav
 sudo useradd -g clamav -s /bin/false -c "Clam Antivirus" clamav
 sudo chown -R clamav:clamav /usr/share/clamav
-sudo cp /usr/etc/freshclam.conf.sample /usr/etc/freshclam.conf
-sudo cp /usr/etc/clamd.conf.sample /usr/etc/clamd.conf
+sudo cp /etc/freshclam.conf.sample /etc/freshclam.conf
+sudo cp /etc/clamd.conf.sample /etc/clamd.conf
