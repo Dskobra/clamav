716dbbf376d6f6e19e5b546b2a4ce0237f6eb949
Dskobra <jbttoms@gmail.com>
2021-01-20 06:07:08 -0600

    added script to install build dependencies.
    Signed-off-by: Dskobra <jbttoms@gmail.com>

---
+++ install-deps.sh
@@ -0,0 +1,4 @@
+# installs the build dependencies.
+sudo apt-get install -y debhelper build-essential openssl libssl-dev libcurl4-openssl-dev
+sudo apt-get install -y zlib1g-dev libpng-dev libxml2-dev libjson-c-dev libbz2-dev libpcre3-dev
+sudo apt-get install -y libncurses-dev libmilter1.0.1 libmilter-dev valgrind check pkg-config
